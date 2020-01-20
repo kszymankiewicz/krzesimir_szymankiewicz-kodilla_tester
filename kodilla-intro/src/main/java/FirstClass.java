6d9e05b97e6dad5f2a3895b2f95c2e4de989e414
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-01-20 19:09:20 +0100

    LeapYear

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -22,6 +22,9 @@ public class FirstClass {
         Notebook businessLaptop = new Notebook(500, 900, 2017);
         check(businessLaptop);
 
+       LeapYear xYear = new LeapYear(3433);
+       xYear.checkLeapYear();
+
 
     }
     private static void check (Notebook notebook) {
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,22 @@
+public class LeapYear {
+    private int xYear;
+    boolean leap;
+
+    public LeapYear(int xYear) {
+        this.xYear = xYear;
+    }
+    public int getXYear() {
+        return xYear;
+    }
+
+    public void checkLeapYear() {
+        if (xYear % 4 == 0 || xYear % 100 == 0 || xYear % 400 == 0) {
+            leap = true;
+            System.out.println(" Rok jest przystępny! ");
+        } else {
+            leap = false;
+            System.out.println(" Rok nie jest przystępny! ");
+
+        }
+    }
+}

9bc76ff2088315b41ad0e67569448e0fef4a0397
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-01-25 14:10:01 +0100

    dziedziczenie

---
+++ kodilla-basic--tests/src/main/java/com/kodilla/inheritance/homework/Application.java
@@ -0,0 +1,19 @@
+package com.kodilla.inheritance.homework;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    OperatingSystem system = new OperatingSystem(1988);
+    system.getYearProduction();
+    system.turnOn();
+
+    Linux linux = new Linux(1975);
+    linux.getYearProduction();
+    linux.turnOff();
+
+    Windows windows = new Windows(1985);
+    windows.turnOn();
+    windows.getYearProduction();
+}
+}
--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Application.java
@@ -1,7 +1,5 @@
 package com.kodilla.inheritance.homework;
 
-import com.kodilla.inheritance.Car;
-
 public class Application {
     public static void main(String[] args) {
 

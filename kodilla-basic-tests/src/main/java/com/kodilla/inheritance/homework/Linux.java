f40b8769ba098ca6cf8233274020f9355aa57564
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-01-25 13:31:29 +0100

    kodilla basic tests

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Linux.java
@@ -0,0 +1,7 @@
+package com.kodilla.inheritance.homework;
+
+public class Linux extends OperatingSystem {
+    public Linux(int yearProdution) {
+        super(yearProdution);
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Windows.java
@@ -0,0 +1,7 @@
+package com.kodilla.inheritance.homework;
+
+public class Windows extends OperatingSystem {
+    public Windows(int yearProdution) {
+        super(yearProdution);
+    }
+}

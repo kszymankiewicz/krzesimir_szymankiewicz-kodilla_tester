8bb847dedb8de41114601da9e50963b0c52eee73
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-01-25 14:12:19 +0100

    poprawki 2

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Linux.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.inheritance.homework;
-
-public class Linux extends OperatingSystem {
-    public Linux(int yearProdution) {
-        super(yearProdution);
-    }
-}
--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/Windows.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.inheritance.homework;
-
-public class Windows extends OperatingSystem {
-    public Windows(int yearProdution) {
-        super(yearProdution);
-    }
-}

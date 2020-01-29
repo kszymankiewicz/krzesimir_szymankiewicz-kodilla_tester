1de302bd57a88d688a4f2ca54d634e923b800055
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-01-29 21:11:15 +0100

    random shape

---
+++ kodilla-collections/src/main/java/com/kodilla/colections/arrays/ShapeUtils.java
@@ -0,0 +1,29 @@
+package com.kodilla.colections.arrays;
+
+import com.kodilla.colections.interfaces.Circle;
+import com.kodilla.colections.interfaces.Shape;
+import com.kodilla.colections.interfaces.Square;
+import com.kodilla.colections.interfaces.Triangle;
+
+public class ShapeUtils {
+    public static void displayShapeInfo(Shape shape) {
+        System.out.println("----------------");
+        System.out.println("Shape kind: " + getShapeName(shape));
+        System.out.println("Shape area: " + shape.getArea());
+        System.out.println("Shape perimeters: " + shape.getPerimeter());
+
+
+    }
+
+    private static String getShapeName(Shape shape) {
+        if (shape instanceof Circle)
+            return "Circle";
+        else if (shape instanceof Triangle)
+            return "Triangle";
+        else if (shape instanceof Square)
+            return "Square";
+        else
+            return "Uknow shape name";
+
+    }
+}

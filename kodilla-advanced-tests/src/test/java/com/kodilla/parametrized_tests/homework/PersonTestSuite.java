5cad3a88cd9dda5114609c672c583b43028a958b
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-04-16 18:54:03 +0200

    homework 7.3.new

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,16 +1,18 @@
 package com.kodilla.parametrized_tests.homework;
 
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTestSuite {
     @ParameterizedTest
-    @MethodSource(value ="com.kodilla.parametrized_tests.homework.BmiSource#providedData")
-    public void shouldGetBMI(double height, double weight, String expected) {
-        Person person = new Person(height, weight);
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.BmiSource#providedData")
+    public void shouldGetBMI(Person person, String expected) {
         assertEquals(expected, person.getBMI());
     }
+
 
 }

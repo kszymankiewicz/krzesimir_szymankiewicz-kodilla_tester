ebbb31cd5f441dfebcda33997343f8549c160616
Krzesimir Szymankiewicz <kszymankiewicz@gmail.com>
2020-04-26 13:34:55 +0200

    homework 13.4.

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
@@ -1,36 +1,40 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
+
+import org.apache.tools.ant.types.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import java.awt.*;
-
-
-
 
 public class AllegroTestApp {
-    public static void main(String[] args) throws InterruptedException, AWTException {
+    public static void main(String[] args) {
         String driverPath = "/Users/krzesim/IdeaProjects/kodilla-course/kodilla-selenium/src/main/resources/chromedriver";
         System.setProperty("webdriver.chrome.driver", driverPath);
         WebDriver driver = new ChromeDriver();
-        driver.get("http://www.allegro.pl");
-        Thread.sleep(2000);
+        driver.get("https://www.allegro.pl");
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        while (!driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]")).isDisplayed()) ;
+        WebElement alert = driver.findElement(By.xpath("//button[@class=\"_13q9y _8hkto _11eg6 _7qjq4 _ey68j\"]"));
+        alert.click();
 
-        Thread.sleep(2000);
-        WebElement inputField = driver.findElement(By.xpath("//*[@class=\"_d25db_3K7x6 _14uqc\"]"));
+        WebElement inputField = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/input"));
         inputField.sendKeys("Mavic mini");
 
-        WebElement category = driver.findElement(By.xpath("//*[@class=\"_d25db_2P-5I _1h7wt _k70df _7qjq4\"]"));
+        WebElement category = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/div[2]/div/select"));
         Select categorySelect = new Select(category);
-        categorySelect.selectByIndex(2);
+        categorySelect.selectByIndex(3);
+        WebElement button = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_UtYE1\"]/button"));
+        button.click();
+        String url = driver.getCurrentUrl();
+        if (!url.equals("https://allegro.pl/kategoria/elektronika?string=Mavic%20mini&bmatch=baseline-product-cl-eyesa2-engag-dict43-ele-1-1-0318")) {
+            throw new RuntimeException();
 
+        }
         driver.quit();
+
+
     }
 }
